8f2fce36ae5d1aceccc6a8faddc49c95de23c305
Dinora <khalikov7374@gmail.com>
2024-03-13 16:59:27 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Rect from 'react'
+// import { Routes, Rout } from "react-router-dom";
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+    <>
+      {/* <Routes>
+
+      </Routes> */}
+      <Navbar />
+      <h1>lll</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <>
+            <h1>hi</h1>
+        </>
+    );
+};
+
+export default index;
